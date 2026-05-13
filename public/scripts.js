8304f7f2cfcc1ab2f1de8c470c03a63aaaa7fddc
Raoul de Colombel <raoul.decolombel@yahoo.fr>
2026-05-13 17:27:08 +0200

    ajout JS index + maj evenement

--- public/scripts.js
+++ public/scripts.js
@@ -1,25 +1,66 @@
-jQuery(function($) {
-    var $bandef = $('.bandef'),
-        $row = $bandef.parent(),
-        rowWidth = $row.width(),
-        bandefWidth = 0;
+/* Animation du bandeau d'images de la page d'accueil */
+const track = document.querySelector('.bandef-track');
+const bandef = document.querySelector('.bandef');
 
-    // Calcul de la largeur totale des images
-    $bandef.find('img').each(function() {
-        bandefWidth += $(this).outerWidth(true); // Inclut les marges
+let speed = 1;
+let position = 0;
+let isPaused = false;
+let direction = -1;
+
+/* Dupliquer les images */
+track.innerHTML += track.innerHTML;
+
+function animate() {
+
+    if (!isPaused) {
+        position += speed * direction;
+        if (Math.abs(position) >= track.scrollWidth / 2) {
+            position = 0;
+        }
+        track.style.transform = `translateX(${position}px)`;
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+/* Arrêt défilement hors écran*/
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            isPaused = false;
+        } else {
+            isPaused = true;
+        }
     });
 
-    // Dupliquer les images pour assurer un défilement continu
-    while (bandefWidth < rowWidth * 2) {
-        $bandef.append($bandef.html());
-        bandefWidth += bandefWidth;
-    }
+}, {
+    threshold: 0.1
+});
+observer.observe(bandef);
 
-    // Ajout des styles dynamiques pour l'animation
-    $('head').append('<style>@keyframes marquee { 0% { transform: translateX(0); } 100% { transform: translateX(-' + bandefWidth / 2 + 'px); } }</style>');
+/* Boutons */
+const toggleBtn = document.getElementById('toggle-move');
+toggleBtn.addEventListener('click', () => {
+    isPaused = !isPaused;
+    toggleBtn.textContent = isPaused ? 'Reprendre' : 'Pause';
 });
 
+document.getElementById('speed-up')
+.addEventListener('click', () => {
+    speed += 1;
+});
 
+document.getElementById('speed-down')
+.addEventListener('click', () => {
+
+    if (speed > 1) {
+        speed -= 1;
+    }
+
+});
+
+/* Début de menu burger */
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
